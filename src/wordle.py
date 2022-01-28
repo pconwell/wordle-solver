2a89c64861c139a608eb029173459a89afacc8fe
pconwell <patrick.conwell@gmail.com>
2022-01-28 23:59:37 +0000

    temp save

--- src/wordle.py
+++ src/wordle.py
@@ -5,14 +5,31 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from time import sleep
 
+import word_generator
+
+
+# we need to create a template to store our word selections so we
+# can keep track if the letters are green/yellow/grey. 
+# It start empty (obviously).
+# if a character is not in the puzzle = 0
+# if a character is in the puzzle but in the wrong spot = 1
+# if a character is in the puzzle and in the right spot = 2
+word_object = {0: [("",0),("",0),("",0),("",0),("",0)],
+               1: [("",0),("",0),("",0),("",0),("",0)],
+               2: [("",0),("",0),("",0),("",0),("",0)],
+               3: [("",0),("",0),("",0),("",0),("",0)],
+               4: [("",0),("",0),("",0),("",0),("",0)],
+               5: [("",0),("",0),("",0),("",0),("",0)]
+              }
+
 
 def unpack_shadow_dom(shadow_elements):
    """   
-   Wordle uses nested shadow DOMs to... hide stuff?
-   This just unpack the shadow DOMs and return the last element so we can interact with it
+   Wordle website uses nested shadow DOMs to... hide stuff? IDK.
+   This function just unpack the shadow DOMs and return the last element so we can interact with it
 
    Accepts a space delimited string of shadow elements,
-   and assumes that the last element is not a shadow element.   
+   and assumes that the last element is not a shadow element.
    """
 
    elements = [i for i in shadow_elements.split(" ")]
@@ -40,25 +57,30 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.powerlanguage.co.uk/wordle/")
 sleep(1)
 
-# close the pop-up box by "clicking" the game icon
+# close the pop-up box by "clicking" the game icon (aka, the X)
 unpack_shadow_dom("game-app game-modal game-icon")[0].click()
 sleep(1)
 
 
 
 ## word 1
-# enter five letters, then press enter
-word = "wired"
-for letter in word:
+# get a random 5 letter word from the word_generator.py file
+rand_word = word_generator.generate_word(word_object)
+print(f"Picking random word from {rand_word[0]} possible solutions: {rand_word[1]}")
+
+for letter in rand_word[1]:
    unpack_shadow_dom(f'game-app game-keyboard button[data-key="{letter}"]')[0].click()
 unpack_shadow_dom(f'game-app game-keyboard button[data-key="↵"]')[0].click()
 sleep(3) #turning over letters animation takes a while
 
-
-tiles = unpack_shadow_dom(f'game-app game-row[letters="{word}"] game-tile')
-
+# check the letters to see if they are green, yellow, or grey
+# and add them to the word object as appropriate
+tiles = unpack_shadow_dom(f'game-app game-row[letters="{rand_word[1]}"] game-tile')
 for tile in tiles:
    print(tile.text, tile.get_attribute("evaluation"))
+   if tile.get_attribute("evaluation") == "absent":
+      # do something
+      print("absent")
 
 
 
